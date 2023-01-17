de30a85c64b799f63c31220ac7c1cb9c88c616d1
DESKTOP-O0R044J\PC2 <kengzer5@hotmail.com>
2023-01-17 18:15:32 +0700

    update day 2

--- services/handle-follow.js
+++ services/handle-follow.js
@@ -1,8 +1,29 @@
+const { client } = require("../config/line");
+const userService = require("./user/index");
 
-exports.handleFollow = (event)=>{
+exports.handleFollow = async (event) => {
+  const userId = event.source.userId;
+  //console.log("userId คือ ", event.source);
 
- //const userId = event;
+  //get profile user
+  const profile = await client.getProfile(userId);
 
- console.log("userId คือ ", event.source);
-}
+  // console.log(profile);
 
+  //insert profile to table
+  const isExist = await userService.isActiveUser(userId);
+  if (!isExist) { // ถ้าไม่่มี user ให้ทำการเพิ่ม
+    await userService.CreateUser(
+      profile.userId,
+      profile.displayName,
+      profile.pictureUrl
+    );
+  } else {//แต่ถ้ามีแล้วให้ทำการ update ด้วย
+    await userService.updateIsActiveUser(
+      profile.userId,
+      1,
+      profile.displayName,
+      profile.pictureUrl
+    );
+  }
+};
